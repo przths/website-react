2390ed75e080459d98c8dee8655c93259b94c8a0
Prathamesh Alankar Jagtap <prathsjagtap@gmail.com>
2025-03-30 17:47:18 +1100

    Added publishing scripts.

--- src/Common/Links.js
+++ src/Common/Links.js
@@ -1,4 +1,5 @@
-export const CYPHER_IT_GITHUB_URL = "https://github.com/praths7/Ciphersecurity";
-export const PERSONAL_WEBSITE_GITHUB_URL = "https://github.com/praths7/website-react";
-export const WEB_CRAWLER_GITHUB_URL = "https://github.com/praths7/crawler";
+export const CYPHER_IT_GITHUB_URL = "https://github.com/przths/Ciphersecurity";
+export const PERSONAL_WEBSITE_GITHUB_URL = "https://github.com/przths/website-react";
+export const WEB_CRAWLER_GITHUB_URL = "https://github.com/przths/crawler";
+
 export const HYGRAPH_MASTER_URL = "https://ap-south-1.cdn.hygraph.com/content/cm8udyomx028b07uy1krahpun/master";
